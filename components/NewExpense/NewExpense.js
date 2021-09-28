e739ad948f6ae738fd6be7b91bdfc169bad3f4e5
iantuan <ian.tuan@gmail.com>
2021-09-28 19:20:23 +0800

    Adding Form Inputs

---
+++ components/NewExpense/NewExpense.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NewExpense.css';
+
+const NewExpense = () => {
+    return <div clasName = "new-expense">
+        
+    </div>
+}
+
+export default NewExpense;
